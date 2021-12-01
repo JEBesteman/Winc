eeda2ee02172f66a941a23efeb11542b6aadc2d9
Jolanda Besteman <jebesteman@gmail.com>
2021-12-01 16:20:30 +0100

    added print, calculate, variables

---
+++ print/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print("Moed houden Jolanda, even leuk met Python bezig!!")
+print("Wat een rot weer.... morgen zon?")
+print(1, 3, 5, 13, 25)
